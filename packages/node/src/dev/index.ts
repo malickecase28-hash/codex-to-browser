d09c5e17df3695ea10c6e054975b709e44ac2e81
Malicke Case <malickedcase@students.utech.edu.jm>
2026-09-04 16:56:05 -0500

    Export autonomous workflow state machine

--- packages/node/src/dev/index.ts
+++ packages/node/src/dev/index.ts
@@ -2,4 +2,5 @@ export * from "./types.js";
 export * from "./state.js";
 export * from "./visible-browser.js";
 export * from "./orchestrator.js";
+export * from "./autonomous-workflow.js";
 export * from "./plugin-bridge.js";
